dc74eb6e6ae2b72e508ae6efcab0aec9babdd522
ugeHidalgo <ugeHidalgo@hotmail.com>
2016-12-10 10:01:51 +0100

    First apporach to deal with one digit number

--- spec/KataSpec.js
+++ spec/KataSpec.js
@@ -5,4 +5,47 @@ describe('Kata', function() {
     it('should be able to test', function() {
       expect(true).toBeTruthy();
     });
+
+    describe ('numbers between 1 and 9',function () {
+
+        it('should convert 1', function() {
+          expect(solution(1)).toBe('I','1 should be I');
+        });
+
+        it('should convert 2', function() {
+          expect(solution(2)).toBe('II','2 should be II');
+        });
+
+        it('should convert 3', function() {
+          expect(solution(3)).toBe('III','2 should be III');
+        });
+
+        it('should convert 4', function() {
+          expect(solution(4)).toBe('IV','4 should be IV');
+        });
+
+        it('should convert 5', function() {
+          expect(solution(5)).toBe('V','5 should be V');
+        });
+
+        it('should convert 6', function() {
+          expect(solution(6)).toBe('VI','6 should be VI');
+        });
+
+        it('should convert 7', function() {
+          expect(solution(7)).toBe('VII','7 should be VII');
+        });
+
+        it('should convert 8', function() {
+          expect(solution(8)).toBe('VIII','8 should be VIII');
+        });
+
+        it('should convert 9', function() {
+          expect(solution(9)).toBe('IX','9 should be IX');
+        });
+    });
+
+    describe ('for two digits number',function () {
+
+    });
 });
--- src/Kata.js
+++ src/Kata.js
@@ -1,3 +1,25 @@
 function solution(number){
-  // convert the number to a roman numeral
+    var numbers = {},
+        result = "";
+        
+    numbers['1']="I";
+    numbers['5']="V";
+
+    if (number <= 3) {
+      for (var f=1; f<=number; f++) {
+        result+=numbers['1'];
+      }
+    }
+
+    if (number >3 && number < 9 ) {
+      if (number===4) {
+        result+=numbers["1"];
+      }
+      result+=numbers["5"];
+      for (var f=6; f<=number; f++) {
+        result+=numbers['1'];
+      }
+    }
+    
+    return result;
 }
